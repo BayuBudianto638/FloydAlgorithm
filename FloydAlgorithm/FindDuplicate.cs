91c0ab36896d984c17165f9554f0ccb780c5a296
Bayu <radamanthys_ok@yahoo.com>
2022-07-29 09:53:01 +0700

    Add project files.

---
+++ FloydAlgorithm/FindDuplicate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloydAlgorithm
+{
+    public class FindDuplicate
+    {
+        public static int findDuplicate(int[] nums)
+        {
+            var nilai_1 = nums[0];
+            var nilai_2 = nums[0];
+
+            while (true)
+            {
+                nilai_1 = nums[nilai_1];
+                nilai_2 = nums[nums[nilai_2]];
+
+                if (nilai_1 == nilai_2)
+                    break;
+            }
+
+            var ptr1 = nums[0];
+            var ptr2 = nilai_1;
+
+            while (ptr1 != ptr2)
+            {
+                ptr1 = nums[ptr1];
+                ptr2 = nums[ptr2];
+            }
+
+            return ptr1;
+        }
+    }
+}
---
+++ FloydAlgorithm/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FloydAlgorithm;
+
+var nums = new int[] { 3, 1, 3, 4, 2 };
+var result = FindDuplicate.findDuplicate(nums);
+Console.Write($"Result : {result} ");
+Console.ReadKey();
+
